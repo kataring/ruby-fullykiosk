013fab454d30e4dcf4170e092332d866474582ae
Noriaki Katayama <kataring@gmail.com>
2023-03-21 00:28:29 +0900

    Refactor list_devices method to accept filter and groupid

--- lib/fullykiosk/endpoint.rb
+++ lib/fullykiosk/endpoint.rb
@@ -5,18 +5,21 @@ module FullyKiosk
     PATH_CLOUD = "/cloud"
     PATH_REMOTE = "/remote"
 
-    def list_devices
-      send_command(PATH_CLOUD, "listDevices")
+    def list_devices(filter: '', groupid: '')
+      params = {}
+      params[:filter] = filter unless filter.empty?
+      params[:groupid] = groupid unless groupid.empty?
+      send_command(PATH_CLOUD, "listDevices", params)
     end
 
     def get_device_info(devid)
-      send_command(PATH_REMOTE, "getDeviceInfo", type: "json", devid: devid)
+      send_command(PATH_REMOTE, "getDeviceInfo", devid: devid)
     end
 
     private
 
     def send_command(path, cmd, **params)
-      request(path, apiemail: @email, apikey: @api_key, cmd: cmd, **params)
+      request(path, apiemail: @email, apikey: @api_key, type: "json", cmd: cmd, **params)
     end
   end
 end
